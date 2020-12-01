060cb3a40a4fb3ca08ff7a2ae56317cd376907d9
Joshua Messitte <68798981+joshmess@users.noreply.github.com>
2020-12-01 17:24:57 -0500

    Add files via upload

--- tcp_traceroute2.py
+++ tcp_traceroute2.py
@@ -1,109 +1,147 @@
+import sys, os
+import argparse
+import time
 import socket
-import random
+import struct
+from scapy.layers.inet import *
 
-__all__ = ['Tracer']
+# Author: Josh Messitte (811976008)
+# CSCI 6760 Project 4: tcp_traceroute.py
+# Usage: sudo python3 tcp_traceroute.py [-m MAX_HOPS] [-p DST_PORT] [-t TARGET]
+
+TIMEOUT = 0.25
+SOCKET_TIMEOUT = 0
+DST_REACHED = 1
+DST_UNREACHABLE = 2
+ICMP_ECHO_REQUEST = 8
 
 
-class Tracer(object):
-    def __init__(self, dst, hops=30):
-        """
-        Initializes a new tracer object
+# receives the echo from the target, returns delay
+def rcv_ping(icmp_socket):
+    start = time.time()
 
-        Args:
-            dst  (str): Destination host to probe
-            hops (int): Max number of hops to probe
-
-        """
-        self.dst = dst
-        self.hops = hops
-        self.ttl = 1
-
-        # Pick up a random port in the range 33434-33534
-        self.port = random.choice(range(33434, 33535))
-
-    def run(self):
-        """
-        Run the tracer
-
-        Raises:
-            IOError
-
-        """
-        try:
-            dst_ip = socket.gethostbyname(self.dst)
-        except socket.error as e:
-            raise IOError('Unable to resolve {}: {}', self.dst, e)
-
-        text = 'traceroute to {} ({}), {} hops max'.format(
-            self.dst,
-            dst_ip,
-            self.hops
-        )
-
-        print(text)
-
-        while True:
-            receiver = self.create_receiver()
-            sender = self.create_sender()
-            sender.sendto(b'', (self.dst, self.port))
-
-            addr = None
-            try:
-                data, addr = receiver.recvfrom(1024)
-            except socket.error as e:
-                raise IOError('Socket error: {}'.format(e))
-            finally:
-                receiver.close()
-                sender.close()
-
-            if addr:
-                print('{:<4} {}'.format(self.ttl, addr[0]))
-            else:
-                print('{:<4} *'.format(self.ttl))
-
-            self.ttl += 1
-
-            if addr[0] == dst_ip or self.ttl > self.hops:
-                break
-
-    def create_receiver(self):
-        """
-        Creates a receiver socket
-
-        Returns:
-            A socket instance
-
-        Raises:
-            IOError
-
-        """
-        s = socket.socket(
-            family=socket.AF_INET,
-            type=socket.SOCK_RAW,
-            proto=socket.IPPROTO_ICMP
-        )
+    while (start + TIMEOUT - time.time()) > 0:
 
         try:
-            s.bind(('', self.port))
-        except socket.error as e:
-            raise IOError('Unable to bind receiver socket: {}'.format(e))
+            icmp_pkt, addr = icmp_socket.recvfrom(1024)
+            # tcp_pkt, addr = tcp_socket.recvfrom(1024)
+        except socket.timeout:
+            break
 
-        return s
+        rcvd_time = time.time()
+        ip = IP(icmp_pkt)
+        icmp = ip[ICMP]
+        # icmp.show()
+        imcp_code = ip[ICMP].code
+        icmp_type = ip[ICMP].type
 
-    def create_sender(self):
-        """
-        Creates a sender socket
+        # ICMP type 11 code 0 --> TIME EXCEEDED
+        if icmp_type == 11 and imcp_code == 0:
+            return (rcvd_time - start), addr, None
+        # ICMP type 0 code 0 --> ECHO REPLY (DST Reached)
+        elif icmp_type == 0 and imcp_code == 0:
+            return (rcvd_time - start), addr, DST_REACHED
+        # ICMP type 3 --> DST UNREACHABLE
+        elif icmp_type == 3:
+            return None, None, DST_UNREACHABLE
 
-        Returns:
-            A socket instance
+    return None, None, SOCKET_TIMEOUT
 
-        """
-        s = socket.socket(
-            family=socket.AF_INET,
-            type=socket.SOCK_DGRAM,
-            proto=socket.IPPROTO_UDP
-        )
 
-        s.setsockopt(socket.SOL_IP, socket.IP_TTL, self.ttl)
+# sends a packet to the target
+def send_probe(s, dst_addr, dst_port, id, ttl):
 
-        return s
+    syn_pkt = IP(dst=dst_addr, ttl=ttl) / TCP(dport=dst_port, sport=54321, flags='S')
+    s.sendto(bytes(syn_pkt), (dst_addr, dst_port))
+
+
+# controls flow for performing one ping
+def perform_ping(dst_addr, dst_port, ttl):
+
+    # sending socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
+
+    # receiving sockets
+    recv_icmp = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+    recv_tcp = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+    recv_icmp.settimeout(2)
+    recv_tcp.settimeout(2)
+
+    # store current process ID
+    id = os.getpid() & 0xFFFF
+
+    send_probe(s, dst_addr, dst_port, id, ttl)
+
+    delay = rcv_ping(recv_icmp)
+    recv_icmp.close()
+
+    return delay
+
+
+# prints results of a ping
+def print_part(delay, address, prev_addr):
+    if not delay:
+        print('*', end=' ', flush=True)
+        return
+
+    # to ms
+    delay *= 1000
+
+    if not prev_addr == address:
+        try:
+            host, _, _ = socket.gethostbyaddr(address)
+        except:
+            host = address
+
+        print('{} ({})  {:.3f} ms'.format(host, address, delay),
+              end=' ', flush=True)
+    else:
+        print(' {:.3f} ms'.format(delay),
+              end=' ', flush=True)
+
+
+# controls main flow of program
+def traceroute(max_hops, dst_port, dst_host, dst_addr):
+    # iterate over all ttls
+    for ttl in range(1, max_hops + 1):
+
+        print('{:2} '.format(ttl), end=' ', flush=True)
+        prev_addr = None
+
+        # compute latency 3 times
+        for i in range(3):
+            delay, address, info = perform_ping(dst_addr, dst_port, ttl)
+            print_part(delay, address, prev_addr)
+            prev_addr = address
+
+        print()
+
+        if info == DST_REACHED:
+            break
+
+
+def main():
+    # Set up argument parsing automation
+    prog = 'python3 tcp_traceroute.py'
+    descr = 'TCP Traceroute Program Implemented in Python'
+    parser = argparse.ArgumentParser(prog=prog, description=descr)
+    parser.add_argument('-m', '--MAX_HOPS', type=int, default=30, required=False, help='Max hops to probe')
+    parser.add_argument('-p', '--DST_PORT', type=int, default=80, help='TCP Destination Port')
+    parser.add_argument('-t', '--TARGET', type=str, required=True, help='Target domain or IP')
+
+    # Parse given arguments
+    args = parser.parse_args()
+    max_hops = args.MAX_HOPS
+    dst_port = args.DST_PORT
+    target = args.TARGET
+
+    dest = socket.gethostbyname(target)
+
+    print("traceroute to %s (%s), %d hops max" % (target, dest, max_hops))
+
+    traceroute(max_hops, dst_port, target, dest)
+
+
+if __name__ == '__main__':
+    main()
